6121317a5290c598974d365bb18d06358aae3405
Fryaner1 <you@example.com>
2025-02-12 17:57:13 +0300

    update: Добавил меню адаптив хидер

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 
+const isActive = ref(false)
+
+function openBurgerMenu() {
+    isActive.value = !isActive.value
+    console.log('f')
+}
 </script>
 
 <template>
@@ -12,8 +19,8 @@ import { RouterLink } from 'vue-router';
                 <h2 class="header__sub-title">Лучшие в своём деле</h2>
             </div>
         </div>
-        <nav class="header__nav">
-            <ul class="header__list">
+        <nav class="header__nav ">
+            <ul class="header__list" :class="{active: isActive}">
                 <li class="header__link"><RouterLink to="">Главная</RouterLink></li>
                 <li class="header__link"><RouterLink to="">О компании</RouterLink></li>
                 <li class="header__link"><RouterLink to="">Контакты</RouterLink></li>
@@ -21,17 +28,17 @@ import { RouterLink } from 'vue-router';
                 <li class="header__link"><RouterLink to="">Доставка и оплата</RouterLink></li>
             </ul>
         </nav>
-        <div class="header__contacts">
+        <div class="header__contacts" :class="{active: isActive}">
             <div class="header__phone-mail">
                 <a class="header__mail" href="mailto:ooo.makrus@mail.ru">ooo.makrus@mail.ru</a>
                 <a class="header__phone" href="tel:+79778833882">+ 7  977 883 38 82</a>
             </div>
             <button class="header__btn">Заказать звонок специалиста</button>
         </div>
-        <div class="burger">
-            <p class="burger__top"></p>
-            <p class="burger__middle"></p>
-            <p class="burger__bottom"></p>
+        <div class="burger" @click="openBurgerMenu">
+            <p class="burger__top" :class="{active: isActive}"></p>
+            <p class="burger__middle" :class="{active: isActive}"></p>
+            <p class="burger__bottom" :class="{active: isActive}"></p>
         </div>
     </header>
 </template>
@@ -47,7 +54,29 @@ import { RouterLink } from 'vue-router';
         display: block;
     }
 
+    @media screen and (max-width: $mobile-max-width){
+        &__top {
+            &.active {
+                top: 8px;
+                transform: rotate(45deg);
+            }
+        }
+        &__middle {
+            &.active {
+                display: none;
+            }
+        }
+        &__bottom {
+            &.active {
+                bottom: 8px;
+                transform: rotate(-45deg);
+            }
+        }
+    }
+
     &__top, &__middle, &__bottom {
+        transition: .5s;
+        transform: rotate(0);
         left: 0;
         content: '';
         position: absolute;
@@ -88,23 +117,30 @@ import { RouterLink } from 'vue-router';
         display: flex;
         gap: 23px;
         font-size: 14px;
+        transition: .5s;
 
-        // @media screen and (max-width: $tablet-max-width){
-        //     position: absolute;
-        //     right: 0;
-        //     flex-direction: column;
-        //     width: 320px;
-        //     height: 100%;
-        //     top: 0;
-        // }
+        &.active {
+            transform: translateX(0);
+        }
 
         @media screen and (max-width: $desctop-small-max-width){
             font-size: 12px;
             gap: 20px;
         }
-        
-        @media screen and (max-width: $desctop-small-max-width){
-            font-size: 12px;
+
+        @media screen and (max-width: $mobile-max-width){
+            padding-top: 100px;
+            display: flex;
+            position: absolute;
+            right: 0;
+            flex-direction: column;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            background-color: $color-white;
+            align-items: center;
+            gap: 20px;
+            transform: translateX(100%);
         }
 
         a {
@@ -126,7 +162,9 @@ import { RouterLink } from 'vue-router';
         a {
             transition: .5s;
         }
-        &:hover {
+
+        @media (hover: hover) {
+            &:hover {
             &::after {
                 width: 100%;
             }
@@ -134,19 +172,48 @@ import { RouterLink } from 'vue-router';
             color: $color-dark-blue;
             }
         }
+        }
+
+        // @media screen and (max-width: $desctop-small-max-width){
+        //     font-size: 20px;
+        // }
+        
+
+        @media screen and (max-width: $mobile-max-width){
+            padding-bottom: 20px;
+            border-bottom: 1px solid $color-black;
+            width: 100%;
+            text-align: center;
+        }
     }
     &__contacts {
         display: flex;
         gap: 20px;
 
-        // @media screen and (max-width: $tablet-max-width){
-        //     position: absolute;
-        //     right: 0;
-        //     flex-direction: column;
-        //     width: 320px;
-        //     bottom: 0;
-        //     align-items: center;
-        // }
+        transition: .5s;
+
+        &.active {
+            transform: translateX(0) translateY(-150%);
+        }
+
+        @media screen and (max-width: $tablet-max-width){
+            position: absolute;
+            right: 0;
+            flex-direction: column;
+            width: 100%;
+            top: 300px;
+            align-items: center;
+        }
+
+        @media screen and (max-width: $mobile-max-width){
+            transform: translateX(100%) translateY(-150%);
+            position: absolute;
+            right: 0;
+            flex-direction: column;
+            width: 100%;
+            top: 100vh;
+            align-items: center;
+        }
     }
     &__phone-mail {
         display: flex;
