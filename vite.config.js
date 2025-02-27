7bc16733d0400d2c31410e54ba6788e0f4b09be7
Ilya <ilya.orlov123@mail.ru>
2025-02-28 04:19:56 +1000

    Добавил карту сайта и robots

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist', // Стандартное значение, убедитесь, что у вас нет другого
+  }
 })
